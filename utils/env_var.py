daa8b6c3a4773828452e47cc9dba5aeac068e9c7
Mejri Rached <r.mejri74100@gmail.com>
2020-10-18 18:52:55 +0200

    feat: impl a function to get env var. with custom exception

---
+++ utils/env_var.py
@@ -0,0 +1,11 @@
+from os import environ
+from .custom_exception import EnvironnementVariableNotFound
+
+def get_env_var(variable_name):
+    env_var = environ.get(variable_name)
+
+    if env_var is None:
+        raise EnvironnementVariableNotFound(f"You need a {variable_name} environnement variable.")
+
+    return env_var
+
